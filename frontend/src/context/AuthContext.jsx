abc4f418251043ee41636abd75ac9af7e21ef24f
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2026-05-16 20:33:23 +0200

    feat: add auth context with login and logout

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,3 +2,41 @@
 // Exports: useAuth() hook with { user, login(email, password), logout(), loading }
 // login() calls POST /api/auth/login, saves token and user to localStorage
 // logout() clears localStorage and resets user to null
+import { createContext, useContext, useState } from "react";
+import axios from 'axios'
+
+const AuthContext = createContext() // "Empty box"
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [token, setToken] = useState(null)
+  
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/login', {
+      email,
+      password
+    })  
+    setUser(response.data.employee)
+    setToken(response.data.token)
+    localStorage.setItem('token', response.data.token)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const logout = () => {
+    setUser(null)
+    setToken(null)
+    localStorage.removeItem('token')
+  }
+return (
+  <AuthContext.Provider value={{user, token, login, logout}}>
+    {children}
+  </AuthContext.Provider>
+)
+}
+const useAuth = () => {
+  return useContext(AuthContext)
+}
+
+export { AuthProvider, useAuth }
