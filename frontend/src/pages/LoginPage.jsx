993547c13a67d5aaac33e10eb633f07a7c652512
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2026-05-16 21:19:47 +0200

    feat: add login page structure with role tabs and form

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -5,3 +5,63 @@
 // - Email input with envelope icon + inline red error if invalid
 // - Password input with lock icon + inline red error if wrong
 // - "Login to dashboard" button — calls login() from AuthContext, then navigates to /
+
+import { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+
+
+function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('Employee')
+  const [error, setError] = useState(null)
+  const { login } = useAuth()
+  const navigate  = useNavigate()
+
+  const handleSubmit = async () => {
+    try {
+      await login(email, password)
+      navigate('/')        
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        
+        <button onClick={() => setRole('Employee')}>Employee</button>
+        <button onClick={() => setRole('Manager')}>Manager</button>
+        <button onClick={() => setRole('Admin')}>Admin</button>
+
+      </div>
+      
+      <div>
+      
+        <input 
+        type="text"
+        placeholder="Email"
+        value={email}
+        required
+        onChange={(e) => setEmail(e.target.value)}
+      />
+        <input 
+          type="password"
+          placeholder="Password"
+          value={password}
+          required
+          minLength={8}
+          onChange={(e) => setPassword(e.target.value)}
+          
+        />
+
+        <button onClick={() => handleSubmit()}>Login</button>
+      </div>
+      
+    </div>
+  )
+} 
+export default LoginPage
