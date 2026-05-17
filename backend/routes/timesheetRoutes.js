dd05022f6b6e8dfaabb9d9a759bed14bc91477a3
hussein hodroy <HusseinH101>
2026-05-17 18:28:09 +0200

    feat: Backend for timesheet complete

--- backend/routes/timesheetRoutes.js
+++ backend/routes/timesheetRoutes.js
@@ -1,17 +1,17 @@
-// Routes for the timesheet, It connects URLs to controller functions
+
 const express = require('express')
 const router = express.Router()
 const { createShift, getMyShifts, getPendingShifts, getAllShifts, updateShiftStatus } = require('../controllers/timesheetController.js')
-const { protect } = require('../middleware/authMiddleware.js') // Person 1's middleware — checks JWT token
-const { restrictTo } = require('../middleware/roleMiddleware.js') // Person 1's middleware — checks user role
+const { authMiddleware } = require('../middleware/authMiddleware.js')
+const { roleMiddleware } = require('../middleware/roleMiddleware.js')
 
-// Employee routes: any logged in user can access 
-router.post('/', protect, createShift)
-router.get('/my', protect, getMyShifts)
+// Employee routes — any logged in user can access these
+router.post('/', authMiddleware, createShift)
+router.get('/my', authMiddleware, getMyShifts)
 
-// Manager routes: only Manager and Admin can access 
-router.get('/pending', protect, restrictTo('Manager', 'Admin'), getPendingShifts)
-router.get('/all', protect, restrictTo('Manager', 'Admin'), getAllShifts)
-router.put('/:id/status', protect, restrictTo('Manager', 'Admin'), updateShiftStatus)
+// Manager routes — only Manager and Admin can access these
+router.get('/pending', authMiddleware, roleMiddleware('Manager', 'Admin'), getPendingShifts)
+router.get('/all', authMiddleware, roleMiddleware('Manager', 'Admin'), getAllShifts)
+router.put('/:id/status', authMiddleware, roleMiddleware('Manager', 'Admin'), updateShiftStatus)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const authRoutes = require('./routes/authRoutes.js')
+const timesheetRoutes = require('./routes/timesheetRoutes.js')
+
 dotenv.config()
 
 const app = express()
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
 app.use('/api/auth', authRoutes)
+app.use('/api/timesheet', timesheetRoutes)
 
 app.get('/', (req, res) => {
   res.json({message: 'Server is running'})
