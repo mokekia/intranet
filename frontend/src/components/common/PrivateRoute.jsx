f5b9db682f4341065c172f41f37484e25840c7a8
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2026-05-17 17:44:32 +0200

    feat: add components for RoleRoute and PrivateRoute components

--- frontend/src/components/common/PrivateRoute.jsx
+++ frontend/src/components/common/PrivateRoute.jsx
@@ -1,1 +1,14 @@
 // Reads user from AuthContext — if not logged in redirects to /login, otherwise renders <Outlet />
+import { useAuth } from '../../context/AuthContext'
+import { Navigate, Outlet} from 'react-router-dom'
+
+function PrivateRoute() {
+  const { user, login, logout, token } = useAuth()
+  if (!user) {
+    return <Navigate to='/login' />
+  }else{
+    return <Outlet />
+  }
+} 
+
+export default PrivateRoute
