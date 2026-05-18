70a8fcc6a31d64f4224007bffac924ff87ae6eb3
Jeyson Nkounga <jeyson.nkounga0001@stud.hkr.se>
2026-05-18 11:04:53 +0200

    feat: The news routes are defined and connected the URLs to the controller functions

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -2,3 +2,18 @@
 // GET    /api/news        → newsController.getNews
 // POST   /api/news        → newsController.createNews    (manager, admin)
 // DELETE /api/news/:id    → newsController.deleteNews    (admin)
+
+import { router } from 'express';
+
+import { getNews, createNews, deleteNews } from '../controllers/newsController';
+import { authMiddleware } from '../middleware/authMiddleware.js';
+
+import { roleMiddleware } from '../middleware/authMiddleware.js';
+
+const router = Router();
+
+router.get('/', authMiddleware, getNews);
+router.post("/", authMiddleware, roleMiddleware(['manager', 'admin']), createNews);
+router.delete("/:id", authMiddleware, roleMiddleware([ 'admin']), deleteNews);
+
+export default router;
